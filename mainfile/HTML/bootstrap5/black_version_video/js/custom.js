5391676282ecf83af7100155a17b0c0090ceb712
Aster1 <gangjinsu4@gmail.com>
2023-12-22 11:37:06 +0900

    12221137

--- mainfile/HTML/bootstrap5/black_version_video/js/custom.js
+++ mainfile/HTML/bootstrap5/black_version_video/js/custom.js
@@ -59,40 +59,40 @@ Assigned to: ThemeForest
 	$('.prt_skills_wrapper').appear(function() {
 		var circle1 = Circles.create({
 			id: 'circles-1',
-			value: 90,
+			value: 100,
 			radius: 100,
-			number: 90,
-			text: '90%',
+			number: 100,
+			text: '100%',
 			width: 8,
 			colors: ["#202020", "#00c8ff"],
 			duration: 900
 		});
 		var circle2 = Circles.create({
 			id: 'circles-2',
-			value: 80,
+			value: 100,
 			radius: 100,
-			number: 80,
-			text: '80%',
+			number: 100,
+			text: '100%',
 			width: 8,
 			colors: ["#202020", "#ff8511"],
 			duration: 900
 		});
 		var circle3 = Circles.create({
 			id: 'circles-3',
-			value: 75,
+			value: 100,
 			radius: 100,
-			number: 75,
-			text: '75%',
+			number: 100,
+			text: '100%',
 			width: 8,
 			colors: ["#202020", "#f26525"],
 			duration: 900
 		});
 		var circle4 = Circles.create({
 			id: 'circles-4',
-			value: 70,
+			value: 100,
 			radius: 100,
-			number: 70,
-			text: '70%',
+			number: 100,
+			text: '100%',
 			width: 8,
 			colors: ["#202020", "#1d8bbe"],
 			duration: 900
